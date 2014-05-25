5595e71a3c010884f1be983793b1904b12dfbc2e
Agacisz <agatabartolik@gmail.com>
2014-05-25 15:06:15 +0200

    zakończenie rpac nad zmiana funkcji dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,4 +1,6 @@
 def dodawanie(l1,l2):
     return l1+l2
-    
-dodawanie()    
+ 
+l1=int(input("Wprowadz pierwszą liczbę "))
+l2=int(input("Wprowadz drugą liczbę "))   
+print(dodawanie(l1,l2))    
